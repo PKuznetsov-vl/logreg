d2dd7ecb3b7f3a1ea68271d47c40bcd0b583123e
pavel <p.kuzetsov@visionlabs.ru>
2022-04-22 16:47:07 +0300

    Add images

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # logreg
 доделать график и ок
+
 https://gist.github.com/fchollet/b7507f373a3446097f26840330c1c378#file-keras_logistic_regression-py
+
+В dev branch классификатор цифр 
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
 from keras import regularizers
+from keras.datasets import mnist
 from matplotlib.cm import get_cmap
 from numpy import linspace, meshgrid, c_
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
-from mlxtend.plotting import plot_decision_regions
+#from mlxtend.plotting import plot_decision_regions
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.model_selection import GridSearchCV
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 
 from tensorflow.keras import layers
 
@@ -147,44 +153,101 @@ def lr():
 
     plot_boundary(logit, X, y, grid_step=.01, poly_featurizer=poly)
 
-def kd_model():
+
+def create_model(x):
+    #Добавляем регуляризацию
+    # создаем модель с бинарной классификацией
+    model = tf.keras.models.Sequential()
+    # 1 слой Выравнивает вход. Не влияет на размер партии.
+    model.add(tf.keras.layers.Flatten())
+    # 2 слой Dense реализует операцию: output = activation(dot(input, kernel) + bias), где активация
+    # — это функция активации по элементам,
+    # переданная в качестве аргумента активации, кернел — это матрица весов, созданная слоем,
+    # а смещение — это вектор смещения, созданный слоем (применимо только в случае, если use_bias — True).
+    model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, input_dim=x.shape[1],
+                                    kernel_regularizer=regularizers.L1L2(l1=1e-1, l2=1e-1),
+                                    bias_regularizer=regularizers.L2(1e-1),
+                                    activity_regularizer=regularizers.L2(1e-1)))
+    # Компилируем модель оптимизатор= rmsprop
+    # функция потерь бинарная энтропия
+
+    return model
+
+
+def binary_model():
     x, y = load_data('ex2data1.txt')
     #полиномиальные признаки
     poly = PolynomialFeatures(degree=7)
     xp = poly.fit_transform(x)
-    #Добавляем регуляризацию
-
-
 
 
     #разбиваем датасет
     train_X, test_X, test_Y, train_Y = train_test_split(xp, y, random_state=15, test_size=0.5)
-    #создаем модель с бинарной классификацией
-    model = tf.keras.models.Sequential()
-    #1 слой Выравнивает вход. Не влияет на размер партии.
-    model.add(tf.keras.layers.Flatten())
-    #2 слой Dense реализует операцию: output = activation(dot(input, kernel) + bias), где активация
-    # — это функция активации по элементам,
-    # переданная в качестве аргумента активации, кернел — это матрица весов, созданная слоем,
-    # а смещение — это вектор смещения, созданный слоем (применимо только в случае, если use_bias — True).
-    model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid, input_dim=xp.shape[1],
-                                    kernel_regularizer=regularizers.L1L2(l1=1e-1, l2=1e-1),
-    bias_regularizer=regularizers.L2(1e-1),
-    activity_regularizer=regularizers.L2(1e-1)))
-    #Компилируем модель оптимизатор= rmsprop
-    #функция потерь бинарная энтропия
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy')
-    #тренируем модель  100 эпох
+
+    model=create_model(train_X)
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy',metrics=['binary_accuracy'])
+    # тренируем модель  100 эпох
     model.fit(train_X, train_Y, epochs=100, verbose=1)
     #выводим предсказания
     ans = model.predict(test_X)
     print("Предсказания")
     print(ans.ravel())
-    #fig, ax = plot_decision_boundary(X=x, y=y, model=model,poly_featurizer=poly)
+    fig, ax = plot_decision_boundary(X=x, y=y, model=model,poly_featurizer=poly)
+    fig.savefig("output.png")
+
+
+
+
+def getmnist():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train.shape
+
+    y_train.shape
+    print(y_train[0:10])
+    x_train_new, y_train_new = x_train[(y_train == 0) | (y_train == 1)], y_train[(y_train == 0) | (y_train == 1)]
+    x_train_final = x_train_new.reshape((-1, 784))
+    x_train_final.shape
+    x_test_new, y_test_new = x_test[(y_test == 0) | (y_test == 1)], y_test[(y_test == 0) | (y_test == 1)]
+    x_test_new.shape
+    x_test_final = x_test_new.reshape((-1, 784))
+    x_train_final = x_train_final / 255
+    x_test_final = x_test_final / 255
+    return  x_train_final,x_test_final,y_train_new,y_test_new
+
+def convert_image(file):
+  image = np.array(Image.open(file).convert('L'))
+  return np.abs(((image / 255) - 1)*(-1))
+
+
+
+def digits_class_low():
+    x_train,x_test,y_train_new,y_test_new=getmnist()
+    model=create_model(x_train)
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['binary_accuracy'])
+    # тренируем модель  100 эпох
+    model.fit(x_train, y_train_new, epochs=100, verbose=1)
+    #смотрим точность обучения
+    print(model.metrics_names)
+    print(model.evaluate(x_test))
+    # выводим предсказания
+    ans = model.predict(x_test)
+    print("Предсказания")
+    print(ans.ravel())
+    model.save(r'./logisticRegressionKeras.hdf5')
+    #fig, ax = plot_decision_boundary(X=x, y=y, model=model, poly_featurizer=poly)
     #fig.savefig("output.png")
 
+if __name__ == '__main__':
+    #digits_class_low()
+    model = tf.keras.models.load_model(r'./logisticRegressionKeras.hdf5')
+    im = convert_image('moje-zero.png')
+    plt.imshow(im)
+    #lr()
+    #binary_model()
+    predict_input = im.reshape((-1, 784))
+    prediction = model.predict(predict_input)
+    print(prediction)
 
 
 
-#lr()
-kd_model()
+
